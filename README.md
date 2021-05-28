76bf4d113b60835d0008b4bec173c2eb6812b75f
Abinash Panda <abinash@prodios.com>
2021-05-28 11:09:03 +0530

    feat(readme) update readme document

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Ray tracing using rust
 
 Implementation of ray tracing (https://raytracing.github.io/books/RayTracingInOneWeekend.html) using `rust`.
+
+## Output
+
+Run `cargo run --release` to generate the ray-traced image.
+
+<img src="./assets/output.png" width="600px" />
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 extern crate image;
 
+use std::fs::create_dir;
+use std::path::Path;
 use std::time::Instant;
 
 use image::{ImageBuffer, RgbImage};
@@ -98,51 +100,15 @@ fn main() {
         10.0,
     );
 
-    // let material_ground = LambertMaterial {
-    //     color: Vec3::new(0.1, 0.1, 0.1),
-    // };
-    // let material_left = LambertMaterial {
-    //     color: Vec3::new(0.6, 0.2, 0.01),
-    // };
-    // let material_center = DielectricMaterial {
-    //     color: Vec3::new(0.8, 0.3, 0.02),
-    //     refraction_index: 1.1,
-    // };
-    // let material_right = MetalMaterial {
-    //     color: Vec3::new(0.6, 0.2, 0.01),
-    //     fuzz: 0.0,
-    // };
-
-    // let mut world = HittableList::new();
-    // world.add_object(Box::new(Sphere::new(
-    //     (0.0, -100.5, -1.0),
-    //     100.0,
-    //     Box::new(material_ground),
-    // )));
-    // world.add_object(Box::new(Sphere::new(
-    //     // move the sphere slightly in the y-axis to prevevent weird dot generated at the point
-    //     // of intersetction
-    //     // TODO Fix the weird dot generated at the bottom
-    //     (0.0, 0.001, -1.0),
-    //     0.5,
-    //     Box::new(material_center),
-    // )));
-    // world.add_object(Box::new(Sphere::new(
-    //     (-1.05, 0.0, -1.25),
-    //     0.5,
-    //     Box::new(material_left),
-    // )));
-    // world.add_object(Box::new(Sphere::new(
-    //     (0.25, 0.0, -2.0),
-    //     0.5,
-    //     Box::new(material_right),
-    // )));
-
     let world = make_scene();
 
     let now = Instant::now();
     ray_trace(&camera, &world, &mut img);
     println!("ray tracing took {:.2?}", now.elapsed());
+
+    if !Path::new("output").exists() {
+        create_dir("output").unwrap();
+    }
 
     img.save("output/ray_traced_image.png").unwrap();
 }
