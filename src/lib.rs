c752cc780522f97c7a6495e0450ab43386d25f23
Abinash Panda <abinash@prodios.com>
2021-05-31 16:46:31 +0530

    feat(multi-threading) divide the image generation into multiple chunks

--- src/lib.rs
+++ src/lib.rs
@@ -25,6 +25,7 @@ pub const IMAGE_HEIGHT: u32 = ((IMAGE_WIDTH as f64) / IMAGE_ASPECT_RATIO) as u32
 pub const SAMPLES_PER_PIXEL: u32 = 20;
 const PB_INCREMENT: u32 = 1000;
 const MAX_RAYS: u8 = 50;
+const NUM_CHUNKS: u8 = 4;
 
 const SKY_START_COLOR: Vec3 = Vec3 {
     x: 0.5,
@@ -46,30 +47,41 @@ pub fn ray_trace(camera: &Camera, world: &HittableList, img: &mut RgbImage) {
         ProgressBar::new((IMAGE_WIDTH * IMAGE_HEIGHT * SAMPLES_PER_PIXEL / PB_INCREMENT) as u64);
     let mut count = 0;
 
-    for i in 1..IMAGE_WIDTH {
-        for mut j in 1..IMAGE_HEIGHT {
-            let mut color = Vec3::zero();
-            for _ in 0..SAMPLES_PER_PIXEL {
-                count += 1;
+    let chunk_size_width = IMAGE_WIDTH / (NUM_CHUNKS as u32);
+    let chunk_size_height = IMAGE_HEIGHT / (NUM_CHUNKS as u32);
+    for chunk_x in 0..NUM_CHUNKS {
+        for chunk_y in 0..NUM_CHUNKS {
+            let start_x = (chunk_size_width * (chunk_x as u32)).max(1);
+            let end_x = (start_x + chunk_size_width).min(IMAGE_WIDTH);
+            let start_y = (chunk_size_height * (chunk_y as u32)).max(1);
+            let end_y = (start_y + chunk_size_height).min(IMAGE_HEIGHT);
 
-                let random_num: f64 = rng.gen();
+            for i in start_x..end_x {
+                for mut j in start_y..end_y {
+                    let mut color = Vec3::zero();
+                    for _ in 0..SAMPLES_PER_PIXEL {
+                        count += 1;
 
-                let u = (i as f64 + random_num) / ((IMAGE_WIDTH as f64) - 1.0);
-                let v = (j as f64 + random_num) / ((IMAGE_HEIGHT as f64) - 1.0);
+                        let random_num: f64 = rng.gen();
 
-                // limit the number rays to be 10
-                let mut depth: u8 = MAX_RAYS;
-                let ray = camera.get_origin_ray(u, v);
-                color = color + ray_color(&ray, &world, &mut depth);
+                        let u = (i as f64 + random_num) / ((IMAGE_WIDTH as f64) - 1.0);
+                        let v = (j as f64 + random_num) / ((IMAGE_HEIGHT as f64) - 1.0);
 
-                if count % PB_INCREMENT == 0 {
-                    pb.inc();
+                        // limit the number rays to be 10
+                        let mut depth: u8 = MAX_RAYS;
+                        let ray = camera.get_origin_ray(u, v);
+                        color = color + ray_color(&ray, &world, &mut depth);
+
+                        if count % PB_INCREMENT == 0 {
+                            pb.inc();
+                        }
+                    }
+
+                    // subtract IMAGE_HEIGHT - j as the we want to move the origin from top left to bottom left
+                    j = IMAGE_HEIGHT - j;
+                    write_color(img, i as u32, j as u32, &color, SAMPLES_PER_PIXEL);
                 }
             }
-
-            // subtract IMAGE_HEIGHT - j as the we want to move the origin from top left to bottom left
-            j = IMAGE_HEIGHT - j;
-            write_color(img, i as u32, j as u32, &color, SAMPLES_PER_PIXEL);
         }
     }
     pb.finish_print("done");
